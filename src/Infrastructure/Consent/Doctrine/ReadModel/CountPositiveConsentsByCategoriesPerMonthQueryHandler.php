83ae4e5ec4b7c33b3c1eafb3ff08766906a5651d
Tomáš Glawaty <tomasglawaty@icloud.com>
2023-08-30 00:38:15 +0200

    Monthly statistics command
    - added console command `MonthlyStatisticsCommand` (`cmp:monthly-statistics`)
    - added query `CountConsentsByCategoriesPerMonthQuery`
    - added query `CountFullyNegativeConsentsPerMonthQuery`
    - added query `CountFullyPositiveConsentsPerMonthQuery`

---
+++ src/Infrastructure/Consent/Doctrine/ReadModel/CountPositiveConsentsByCategoriesPerMonthQueryHandler.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Consent\Doctrine\ReadModel;
+
+use App\Domain\Consent\Event\ConsentCreated;
+use App\Domain\Consent\Event\ConsentUpdated;
+use App\ReadModel\Consent\CountConsentsByCategoriesPerMonthQuery;
+use App\ReadModel\Consent\MonthlyStatistics;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use SixtyEightPublishers\ArchitectureBundle\ReadModel\Query\QueryHandlerInterface;
+
+final class CountPositiveConsentsByCategoriesPerMonthQueryHandler implements QueryHandlerInterface
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {}
+
+    public function __invoke(CountConsentsByCategoriesPerMonthQuery $query): MonthlyStatistics
+    {
+        $positiveCategoryFilter = $this->createCategoryFilter($query->acceptedCategories(), true);
+        $negativeCategoryFilter = $this->createCategoryFilter($query->rejectedCategories(), false);
+
+        if ($query->unique()) {
+            $sql = <<<SQL
+            SELECT
+              month,
+              count(*) AS "count"
+            FROM (
+              SELECT DISTINCT ON (_es.aggregate_id, date_part('month', _es.created_at)) date_part('month', _es.created_at) AS month
+              FROM consent_event_stream _es
+              WHERE
+                date_part('year', _es.created_at) = :year
+                AND _es.event_name IN (:eventNames)
+                AND _es.parameters->>'project_id' = :projectId
+                {$positiveCategoryFilter}
+                {$negativeCategoryFilter}
+              ORDER BY _es.aggregate_id, month DESC, _es.created_at DESC
+            ) es
+            GROUP BY month
+            SQL;
+        } else {
+            $sql = <<<SQL
+            SELECT
+              date_part('month', _es.created_at) AS "month",
+              count(*) AS "count"
+            FROM consent_event_stream _es
+            WHERE
+              date_part('year', _es.created_at) = :year
+              AND _es.event_name IN (:eventNames)
+              AND _es.parameters->>'project_id' = :projectId
+              {$positiveCategoryFilter}
+              {$negativeCategoryFilter}
+            GROUP BY month;
+            SQL;
+        }
+
+        $rsm = new ResultSetMappingBuilder($this->em);
+        $rsm->addScalarResult('month', 'month', 'integer');
+        $rsm->addScalarResult('count', 'count', 'integer');
+
+        $data = $this->em->createNativeQuery($sql, $rsm)
+            ->setParameters([
+                'projectId' => $query->projectId(),
+                'year' => $query->year(),
+                'eventNames' => [
+                    ConsentCreated::class,
+                    ConsentUpdated::class,
+                ],
+            ])
+            ->getResult(AbstractQuery::HYDRATE_ARRAY);
+
+        return MonthlyStatistics::fromArray(array_column($data, 'count', 'month'));
+    }
+
+    /**
+     * @param array<int, string> $categories
+     */
+    private function createCategoryFilter(array $categories, bool $positivity): string
+    {
+        return implode(
+            ' ',
+            array_map(
+                static fn (string $categoryCode): string => sprintf(
+                    "AND (_es.parameters->'consents'->>'%s')::boolean = %s",
+                    $categoryCode,
+                    $positivity ? 'true' : 'false',
+                ),
+                $categories,
+            ),
+        );
+    }
+}
---
+++ src/ReadModel/Consent/MonthlyStatistics.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ReadModel\Consent;
+
+/**
+ * @phpstan-type MonthlyStatisticsArray = array{ 1: int, 2: int, 3: int, 4: int, 5: int, 6: int, 7: int, 8: int, 9: int, 10: int, 11: int, 12: int }
+ */
+final class MonthlyStatistics
+{
+    public function __construct(
+        public readonly int $m1,
+        public readonly int $m2,
+        public readonly int $m3,
+        public readonly int $m4,
+        public readonly int $m5,
+        public readonly int $m6,
+        public readonly int $m7,
+        public readonly int $m8,
+        public readonly int $m9,
+        public readonly int $m10,
+        public readonly int $m11,
+        public readonly int $m12,
+    ) {}
+
+    /**
+     * @param array<string|int, int> $array
+     */
+    public static function fromArray(array $array): self
+    {
+        return new self(
+            m1: $array['m1'] ?? $array[1] ?? 0,
+            m2: $array['m2'] ?? $array[2] ?? 0,
+            m3: $array['m3'] ?? $array[3] ?? 0,
+            m4: $array['m4'] ?? $array[4] ?? 0,
+            m5: $array['m5'] ?? $array[5] ?? 0,
+            m6: $array['m6'] ?? $array[6] ?? 0,
+            m7: $array['m7'] ?? $array[7] ?? 0,
+            m8: $array['m8'] ?? $array[8] ?? 0,
+            m9: $array['m9'] ?? $array[9] ?? 0,
+            m10: $array['m10'] ?? $array[10] ?? 0,
+            m11: $array['m11'] ?? $array[11] ?? 0,
+            m12: $array['m12'] ?? $array[12] ?? 0,
+        );
+    }
+
+    /**
+     * @return MonthlyStatistics
+     */
+    public function toArray(): array
+    {
+        return [
+            1 => $this->m1,
+            2 => $this->m2,
+            3 => $this->m3,
+            4 => $this->m4,
+            5 => $this->m5,
+            6 => $this->m6,
+            7 => $this->m7,
+            8 => $this->m8,
+            9 => $this->m9,
+            10 => $this->m10,
+            11 => $this->m11,
+            12 => $this->m12,
+        ];
+    }
+}
